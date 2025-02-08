de246a5920896aeed7b9b774d73d20d60cb68bd5
Axesan <82097764+Axesan@users.noreply.github.com>
2025-02-08 20:43:05 +0100

    Add logik for login and tcheck field value
    - Add field and logik for form login
    - Add message for errors value
    - Add link for api
    
    - 08/02/2025 20H42

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,52 +1,87 @@
-import React from "react";
-import { FormControl, FormHelperText, Input, InputLabel, Button, Box } from "@mui/material";
+import React, { useState, useContext } from "react";
+import { Button, Box, TextField } from "@mui/material";
+
+import { loginUser } from "../api/api";
 
 function Login() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const email = formData.get("email");
-        const password = formData.get("password");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-        console.log("Email:", email);
-        console.log("Mot de passe:", password);
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await loginUser({ email, password });
+      console.log("Connexion réussie !");
+      // Rediriger vers le tableau de bord ou une autre page
+      setError("Connexion reussie !");
+    } catch (err) {
+      setError("Email ou mot de passe incorrect.");
+    }
+  };
 
-    return (
-        <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{
-                maxWidth: 400,
-                margin: "0 auto",
-                padding: "16px",
-                textAlign: "center",
-                display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-            }}
-        >
-            <FormControl fullWidth margin="dense">
-                <InputLabel htmlFor="email" required>
-                    Adresse Email
-                </InputLabel>
-                <Input id="email" name="email" aria-describedby="email-helper" />
-                <FormHelperText id="email-helper">Rentrez votre adresse E-mail.</FormHelperText>
-            </FormControl>
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        maxWidth: 400,
+        margin: "0 auto",
+        padding: "16px",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+    >
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <TextField
+        fullWidth
+        required
+        label="Email"
+        placeholder="Ex: john@example.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        // error={error?.api?.bool === false}
+        // helperText={error?.api?.bool === false ? `${error?.api?.message}` : ""}
+        // slotProps={{
+        //   input: {
+        //     endAdornment:
+        //       error?.api?.bool === false ? (
+        //         <HighlightOffIcon color="error" style={{ padding: "right" }} />
+        //       ) : (
+        //         <></>
+        //       ),
+        //   },
+        // }}
+      />
 
-            <FormControl fullWidth margin="dense">
-                <InputLabel htmlFor="password" required>
-                    Mot de passe
-                </InputLabel>
-                <Input id="password" name="password" type="password" aria-describedby="password-helper" />
-                <FormHelperText id="password-helper">Rentrez votre mot de passe.</FormHelperText>
-            </FormControl>
+      <TextField
+        fullWidth
+        required
+        label="Mot de passe"
+        placeholder="Entrez votre mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        // error={error?.api?.bool === false}
+        // helperText={error?.api?.bool === false ? `${error?.api?.message}` : ""}
+        // slotProps={{
+        //   input: {
+        //     endAdornment:
+        //       error?.api?.bool === false ? (
+        //         <HighlightOffIcon color="error" style={{ padding: "right" }} />
+        //       ) : (
+        //         <></>
+        //       ),
+        //   },
+        // }}
+      />
 
-            <Button type="submit" variant="contained" color="primary">
-                Se connecter
-            </Button>
-        </Box>
-    );
+      <Button type="submit" variant="contained" color="primary">
+        Se connecter
+      </Button>
+    </Box>
+  );
 }
 
 export default Login;
